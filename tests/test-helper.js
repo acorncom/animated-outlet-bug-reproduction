c4310ba1e07d36cf3d0297bb82ef46404eb2c611
David Baker <dbaker@acornwebconsultants.com>
2023-07-27 15:32:33 -0600

    Initial commit

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'animated-outlet-reproduction/app';
+import config from 'animated-outlet-reproduction/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
