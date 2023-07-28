05dd0302909da9eaaab204467af1e040c491006e
David Baker <dbaker@acornwebconsultants.com>
2023-07-27 18:12:31 -0600

    Simplifying rules down to eliminate any possible snags here

--- app/components/animated-outlet.js
+++ app/components/animated-outlet.js
@@ -2,22 +2,9 @@ import Component from '@glimmer/component';
 import fade from 'ember-animated/transitions/fade';
 
 export default class AnimatedOutlet extends Component {
-  rules({ newItems, oldItems }) {
-    const oldRoute = oldItems[oldItems.length - 1];
-    const newRoute = newItems[newItems.length - 1];
-    let oldRouteName = undefined;
-    let newRouteName = undefined;
+  duration = 500;
 
-    if (oldRoute) {
-      oldRouteName = oldRoute.outlets.main.render.name;
-    }
-
-    if (newRoute) {
-      newRouteName = newRoute.outlets.main.render.name;
-    }
-
-    if (oldRouteName !== newRouteName) {
-      return fade;
-    }
-  }
+  rules = () => {
+    return fade;
+  };
 }
